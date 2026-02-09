3f5bdb16b7d9183429b8c817e5badf33baede38d
Sameer Maurya <sameermaurya97044@gmail.com>
2026-02-09 11:40:24 +0530

    feat: Add Node.js script to read `hello.txt` and `abes.txt` files, along with the new text files.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,25 +1,35 @@
-const http = require("http");
+// const http = require("http");
 
-const myServer = http.createServer((req,res)=>{
-    // console.log("Hello World");
-    // res.end("Hi this is my First Server");
-    if(req.url === "/"){
-        res.end("<h1>ABES Engineering College</h1> <br> <img src='https://www.abes.ac.in/assets/2.jpg' alt='ABESEC Logo' width='500' height='350' style='border-radius: 10%; ' border='5px solid black' >");
-    }
-    else if(req.url === "/about"){
-        res.end("We are student of Btech 2nd year");
-    }
-    else if(req.url === "/contact"){
-        res.end("Contact Me at EMAIL");
-    }
-    else if(req.url === '/class'){
-        res.end("<h1>Class IT A</h1>")
-    }
-    else{
-        res.end("404 Not Found");
-    }
-});
+// const myServer = http.createServer((req,res)=>{
+//     // console.log("Hello World");
+//     // res.end("Hi this is my First Server");
+//     if(req.url === "/"){
+//         res.end("<h1>ABES Engineering College</h1> <br> <img src='https://www.abes.ac.in/assets/2.jpg' alt='ABESEC Logo' width='500' height='350' style='border-radius: 10%; ' border='5px solid black' >");
+//     }
+//     else if(req.url === "/about"){
+//         res.end("We are student of Btech 2nd year");
+//     }
+//     else if(req.url === "/contact"){
+//         res.end("Contact Me at EMAIL");
+//     }
+//     else if(req.url === '/class'){
+//         res.end("<h1>Class IT A</h1>")
+//     }
+//     else{
+//         res.end("404 Not Found");
+//     }
+// });
 
-myServer.listen(3000,() =>{
-    console.log("Server Started at port 3000");
-});
+// myServer.listen(3000,() =>{
+//     console.log("Server Started at port 3000");
+// });
+
+const fs = require("fs");
+// fs.writeFileSync("hello.txt","We are student of it");
+// fs.writeFileSync("abes.txt","We are student of ABES");
+
+const result = fs.readFileSync("./hello.txt","utf-8");
+console.log(result);
+
+const result2 = fs.readFileSync("./abes.txt","utf-8");
+console.log(result2);
